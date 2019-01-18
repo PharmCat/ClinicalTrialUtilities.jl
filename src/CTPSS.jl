299b71e172991379ad119acc8486057833ed70ad
PharmCat <riftquod@yandex.ru>
2019-01-18 20:21:15 +0300

    Add files via upload

---
+++ src/CTPSS.jl
@@ -0,0 +1,10 @@
+module TrialSize
+using Distributions
+
+    function OneSampleMean(m0, m1, sd; alpha=0.05, beta=0.8)
+
+        ZDIST = Normal()
+        return (quantile(ZDIST, 1-alpha/2) + quantile(ZDIST, beta))^2*sd^2/(m0-m1)^2
+    end
+
+end # module
