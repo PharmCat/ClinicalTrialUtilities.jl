cb0f496b0b74b7b82e666931f06013547a657c31
PharmCat <v.s.arnautv@yandex.ru>
2019-01-22 01:15:52 +0300

    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,8 @@
-using TrialSize
+using CTPSS
 using Test
+
+    n = ceil(sampleSize(param="mean", type="ea", group="one", alpha=0.05, beta=0.2, sd=1, a=1.5, b=2, k=1))
+    if n == 32 prinln "OK!"
+
+    n = ceil(sampleSize(param="mean", type="ei", group="one", alpha=0.05, beta=0.2, sd=0.1, diff=0.05, a=2, b=2, k=1))
+    if n == 35 prinln "OK!"
