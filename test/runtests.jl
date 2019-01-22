c45d594a9d62184ff79af577728a3b1d1eff3ae7
PharmCat <riftquod@yandex.ru>
2019-01-23 00:45:15 +0300

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 #using CTPSS
 using Test
 
+
     t1 = (32 ≈ ceil(sampleSize(param="mean", type="ea", group="one", alpha=0.05, beta=0.2, sd=1, a=1.5, b=2, k=1)))
     t2 = (35 ≈ ceil(sampleSize(param="mean", type="ei", group="one", alpha=0.05, beta=0.2, sd=0.1, diff=0.05, a=2, b=2, k=1)))
 
