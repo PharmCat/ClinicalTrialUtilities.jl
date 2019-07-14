cbb9fb9e50678ee2589d7ad96f8a2cf71f711df2
PharmCat <v.s.arnautov@yandex.ru>
2019-07-14 20:52:14 +0300

    contab() init

--- src/Freque.jl
+++ src/Freque.jl
@@ -2,9 +2,8 @@
 
 function freque(data::DataFrame; vars::Symbol, alpha = 0.05)::DataFrame
     result = DataFrame(value = Any[], n = Int[], p = Float64[], cil = Float64[], ciu = Float64[])
-    list = unique(data[vars])
-
-    n = length(data[vars])
+    list   = unique(data[vars])
+    n      = length(data[vars])
     for i in list
         ne = count(x -> (x == i), data[vars])
         pe = ne/n
@@ -12,4 +11,20 @@ function freque(data::DataFrame; vars::Symbol, alpha = 0.05)::DataFrame
         push!(result, [i, ne, pe, ci.lower, ci.upper])
     end
     return result
+end
+
+function contab(data::DataFrame; row::Symbol, col::Symbol)::Matrix
+    clist = unique(data[col])
+    rlist = unique(data[row])
+    cn    = length(clist)
+    rn    = length(rlist)
+    dfs   = Array{Int, 2}(undef, rn, cn)
+    for ri = 1:rn
+        rowl  = data[data[row] .== rlist[ri], col]
+        for ci = 1:cn
+            cnt = count(x -> x == clist[ci], rowl)
+            dfs[ri, ci] = cnt
+        end
+    end
+    return dfs
 end
