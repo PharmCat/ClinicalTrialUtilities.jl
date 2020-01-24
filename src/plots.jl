2b5a305aa6dc78cdc4e11b0e1d304317a5597006
PharmCat <v.s.arnautov@yandex.ru>
2020-01-24 19:59:11 +0300

    plots

--- src/plots.jl
+++ src/plots.jl
@@ -1,17 +1,36 @@
 # Clinical Trial Utilities
 # Author: Vladimir Arnautov aka PharmCat
 # Copyright © 2019 Vladimir Arnautov aka PharmCat (mail@pharmcat.net)
-#=
-struct MyType
-    time
-    obs
+
+struct PKPlotStyle
+    linestyle::Symbol
+    linecolor::Symbol
+    markershape::Symbol
+    markercolor::Symbol
 end
 
-@recipe function f(::Type{MyType}, mw::MyType)
-    x = mw.time
-    y = mw.obs
-end
-=#
+const PKPLOTSTYLE = [
+PKPlotStyle(:solid, :blue, :circle, :blue),
+PKPlotStyle(:solid, :red, :utriangle, :red),
+PKPlotStyle(:solid, :green, :diamond, :green),
+PKPlotStyle(:solid, :yellow, :rect, :yellow),
+PKPlotStyle(:solid, :gray, :xcross, :gray),
+PKPlotStyle(:solid, :cyan, :cross, :cyan),
+PKPlotStyle(:solid, :gold, :star, :gold),
+PKPlotStyle(:solid, :magenta, :pentagon, :magenta),
+PKPlotStyle(:solid, :purple, :heptagon, :purple),
+PKPlotStyle(:solid, :indigo, :octagon, :indigo),
+PKPlotStyle(:dot, :blue, :utriangle, :blue),
+PKPlotStyle(:dot, :red, :circle, :red),
+PKPlotStyle(:dot, :green, :rect, :green),
+PKPlotStyle(:dot, :yellow, :diamond, :yellow),
+PKPlotStyle(:dot, :gray, :cross, :gray),
+PKPlotStyle(:dot, :cyan, :xcross, :cyan),
+PKPlotStyle(:dot, :gold, :pentagon, :gold),
+PKPlotStyle(:dot, :magenta, :star, :magenta),
+PKPlotStyle(:dot, :purple, :octagon, :purple),
+PKPlotStyle(:dot, :indigo, :heptagon, :indigo),
+]
 
 @userplot PKPlot
 
@@ -48,11 +67,42 @@ function plot(subj::T; title = nothing) where T <: AbstractSubject
         return pkplot(subj.time, subj.obs;
             title = title,
             )
-        end
+    end
+end
 function plot!(subj::T) where T <: AbstractSubject
     return pkplot!(subj.time, subj.obs;)
 end
 
-function plot(data::DataSet{T}) where T <: AbstractSubject
+function usort(data::DataSet{T}, list) where T <: AbstractData
+    dl = Vector{Dict}(undef, 0)
+    for i in data
+        subd = Dict(k => i.sort[k] for k in list)
+        if subd ∉ dl push!(dl, subd) end
+    end
+    dl
+end
 
+function plot(data::DataSet{T}; pagesort = nothing, typesort = nothing) where T <: AbstractSubject
+    typedict = usort(data, typesort)
+    pagedict = usort(data, pagesort)
+    plots    = Vector{Any}(undef, 0)
+    p        = nothing
+    for i in pagedict
+        fst = true
+        for d in data
+            if i ∈ d.sort
+                #println(i , " --- ", d.sort)
+                if fst
+                    #p = pkplot(d.time, d.obs)
+                    p = plot(d)
+                    fst = false
+                else
+                    #pkplot!(p, d.time, d.obs)
+                    plot!(d)
+                end
+            end
+        end
+        push!(plots, p)
+    end
+    plots
 end
