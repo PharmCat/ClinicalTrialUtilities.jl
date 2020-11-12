b4dfa3af70fdab35633c00103d360b173ff279e7
PharmCat <v.s.arnautov@yandex.ru>
2020-11-12 22:44:51 +0300

    doc update

--- src/ci.jl
+++ src/ci.jl
@@ -162,13 +162,25 @@ Confidence interval for proportion difference.
 
 # Computation methods:
 
-- :nhs - Newcombes Hybrid (wilson) Score interval for the difference of proportions;
-- :nhscc - Newcombes Hybrid Score CC;
-- :ac - Agresti-Caffo interval for the difference of proportions;
-- :mn | :default - Method of Mee 1984 with Miettinen and Nurminen modification;
-- :mee | :fm - Mee maximum likelihood method;
-- :wald - Wald CI without CC;
-- :waldcc - Wald CI with CC;
+- `:nhs` - Newcombes Hybrid (wilson) Score interval for the difference of proportions;
+- `:nhscc` - Newcombes Hybrid Score CC;
+- `:ac` - Agresti-Caffo interval for the difference of proportions;
+- `:mn` | `:default` - Method of Mee 1984 with Miettinen and Nurminen modification;
+- `:mee` | `:fm` - Mee maximum likelihood method;
+- `:wald` - Wald CI without CC;
+- `:waldcc` - Wald CI with CC;
+
+# References
+
+  * nhs, nhscc - Newcombe RG (1998), Interval Estimation for the Difference Between Independent Proportions: Comparison of Eleven Methods. Statistics in Medicine 17, 873-890.
+  * ac - Agresti A, Caffo B., “Simple and effective confidence intervals for proportions and differences of proportions result from adding two successes and two failures”, American Statistician 54: 280–288 (2000)
+  * mn - Miettinen, O. and Nurminen, M. (1985), Comparative analysis of two rates. Statist. Med., 4: 213-226. doi:10.1002/sim.4780040211
+  * mee - Mee RW (1984) Confidence bounds for the difference between two probabilities, Biometrics40:1175-1176
+  * Brown, L.D., Cai, T.T., and DasGupta, A. Interval estimation for a binomial proportion.
+    Statistical Science, 16(2):101–117, 2001.
+  * Farrington, C. P. and Manning, G. (1990), “Test Statistics and Sample Size Formulae for Comparative Binomial Trials with Null Hypothesis of Non-zero Risk Difference or Non-unity Relative Risk,” Statistics in Medicine, 9, 1447–1454
+  * Li HQ, Tang ML, Wong WK. Confidence intervals for ratio of two Poisson rates using the methodof variance estimates recovery. Computational Statistics 2014; 29(3-4):869-889
+  * Brown, L., Cai, T., & DasGupta, A. (2003). INTERVAL ESTIMATION IN EXPONENTIAL FAMILIES. Statistica Sinica, 13(1), 19-49.
 """
 function diffpropci(x1::Int, n1::Int, x2::Int, n2::Int; alpha::Real = 0.05, method::Symbol = :default)::ConfInt
     if alpha >= 1.0 || alpha <= 0.0
